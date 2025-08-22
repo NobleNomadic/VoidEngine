261042eb285d18b56881088b58f0be4be6d18323
NobleNomadic <noblenomadic@proton.me>
2025-08-23 07:55:52 +0930

    Licensed under GPL v2

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,21 @@
+/*
+ * VoidEngine - Simple component based Java game engine
+ *
+ * Copyright (C) 2025 NobleNomadic
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
 // Game.java - Contains main game code
 public class Game {
   public static void main(String[] args) {
